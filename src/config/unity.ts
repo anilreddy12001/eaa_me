018e3edb799779046529fd15a8cd01c283e71158
anilkumarrk <anilreddy12001@gmail.com>
2025-09-03 23:58:55 +0530

    correcting unity build path

--- src/config/unity.ts
+++ src/config/unity.ts
@@ -4,8 +4,8 @@ import { UnityConfig } from '../types/unity';
 //\\INBLRVM61753026\cg ui\public\Build
 // file:\\10.51.109.90\cg ui\public\Build
 export const UNITY_CONFIG: UnityConfig = {
-  loaderUrl: '/energy-analytics/Build/WB.loader.js',//WB2.loader //Build/webgl.loader.js
-  dataUrl: '/energy-analytics/Build/webgl.data',//Build/webgl.data // to be replaced with internal file server url
-  frameworkUrl: '/energy-analytics/Build/build.framework.js',//build.framework.js ///Build/webgl.framework.js
-  codeUrl: '/energy-analytics/Build/build.wasm' //webgl.wasm //https://anilproject12001.web.app/build.wasm //WB.wasm.gz
+  loaderUrl: '/Build/WB.loader.js',//WB2.loader //Build/webgl.loader.js
+  dataUrl: '/Build/webgl.data',//Build/webgl.data // to be replaced with internal file server url
+  frameworkUrl: '/Build/build.framework.js',//build.framework.js ///Build/webgl.framework.js
+  codeUrl: '/Build/build.wasm' //webgl.wasm //https://anilproject12001.web.app/build.wasm //WB.wasm.gz
 };
